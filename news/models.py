135822f97b91efd58c7a80aeb893672350dde8b7
docker <docker@MacBook-Air.local>
2019-05-26 21:48:39 +0300

    asd

--- news/models.py
+++ news/models.py
@@ -7,7 +7,7 @@ from news.lib import modules
 
 # Create your models here.
 class Category(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200, blank=True, null=True)
 
     def __str__(self):
         return self.title
