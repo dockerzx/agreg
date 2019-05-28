398319bbf2e26d2a6b6f9c31666316bb3f22fc41
docker <docker@MacBook-Air.local>
2019-05-28 16:36:23 +0300

    asd

--- news/forms.py
+++ news/forms.py
@@ -1,15 +1,15 @@
 from django import forms
-from .models import Feed, Category
+from .models import Feed#, Category
 
 class FeedForm(forms.ModelForm):
     class Meta:
         model = Feed
-        fields = ['url', 'category']
+        fields = ['url']#, 'category']
         labels = {
             'url': 'RSS/Atom URL',
-            'category': 'Категория'
+            #'category': 'Категория'
         }
         widgets = {
             'url': forms.URLInput(attrs={'class': 'form-control'}),
-            'category': forms.Select(choices=Category.objects.all())
+            #'category': forms.Select(choices=Category.objects.all())
         }
