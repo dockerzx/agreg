27d880f35f433f7ad742feb546e754f9d55d11e9
docker <docker@MacBook-Air.local>
2019-05-30 17:13:22 +0300

    asd

--- newsproject/settings.py
+++ newsproject/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-DJANGO_MODE = 'local'#os.getenv('DJANGO_MODE', "Production").lower()
+DJANGO_MODE = 'production'#os.getenv('DJANGO_MODE', "Production").lower()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
@@ -182,21 +182,21 @@ REST_FRAMEWORK = {
 CORS_ORIGIN_ALLOW_ALL = True
 
 #CELERY STUFF
-if DJANGO_MODE == 'local':
-    BROKER_URL = 'redis://localhost:6379'
-    CELERY_RESULT_BACKEND = 'redis://localhost:6379'
-    CELERY_ACCEPT_CONTENT = ['application/json']
-    CELERY_TASK_SERIALIZER = 'json'
-    CELERY_RESULT_SERIALIZER = 'json'
-    CELERY_TIMEZONE = 'America/Chicago'
-
-if DJANGO_MODE == 'production':
-    BROKER_URL = os.getenv('REDIS_URL')
-    CELERY_RESULT_BACKEND = os.getenv('REDIS_URL')
-    CELERY_ACCEPT_CONTENT = ['application/json']
-    CELERY_TASK_SERIALIZER = 'json'
-    CELERY_RESULT_SERIALIZER = 'json'
-    CELERY_TIMEZONE = 'America/Chicago'
+# if DJANGO_MODE == 'local':
+#     BROKER_URL = 'redis://localhost:6379'
+#     CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+#     CELERY_ACCEPT_CONTENT = ['application/json']
+#     CELERY_TASK_SERIALIZER = 'json'
+#     CELERY_RESULT_SERIALIZER = 'json'
+#     CELERY_TIMEZONE = 'America/Chicago'
+#
+# if DJANGO_MODE == 'production':
+#     BROKER_URL = os.getenv('REDIS_URL')
+#     CELERY_RESULT_BACKEND = os.getenv('REDIS_URL')
+#     CELERY_ACCEPT_CONTENT = ['application/json']
+#     CELERY_TASK_SERIALIZER = 'json'
+#     CELERY_RESULT_SERIALIZER = 'json'
+#     CELERY_TIMEZONE = 'America/Chicago'
 
 # LOGGING
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#logging
